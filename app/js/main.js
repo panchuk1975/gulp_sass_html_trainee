25d4daef787b381ca9e4920bb72399a3fbcff5ed
panchuk1975 <panchuk1975vadik@gmail.com>
2020-07-16 20:15:41 +0300

    benefites is ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,9 @@
 $(function () {
   $(".products_slider").slick({
     prevArrow:
-      '<button class="slider-btn slider-btn__left"><svg width="60" height="48" viewBox="0 0 60 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="60" height="48" fill="white"/><path d="M25.7816 32L34 24L25.7816 16" stroke="black"/></svg></button>',
+      '<button class="slider-btn slider-btn__left"><svg width="60" height="48" viewBox="0 0 60 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M25.7816 32L34 24L25.7816 16"/></svg></button>',
     nextArrow:
-      '<button class="slider-btn slider-btn__right"><svg width="60" height="48" viewBox="0 0 60 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="60" height="48" fill="#1C1C1C"/><path d="M34.2184 16L26 24L34.2184 32" stroke="white"/></svg></button>',
+      '<button class="slider-btn slider-btn__right"><svg width="60" height="48" viewBox="0 0 60 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M34.2184 16L26 24L34.2184 32"/></svg></button>',
+      infinite: false,
   });
 });
